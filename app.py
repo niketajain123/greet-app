b6ed5570c9e4adb813f235c3e29f5e884a10a00a
Niketa Jain <123207030+niketajain123@users.noreply.github.com>
2025-10-13 10:51:08 +0530

    fixed app.py

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def home():
-    name = None
+    fname = None
+    sname= " "
     if request.method == "POST":
         fname = request.form.get("fname")
         sname = request.form.get("sname"," ")
@@ -13,3 +14,4 @@ def home():
 if __name__ == "__main__":
 
     app.run(host="0.0.0.0", port=5000)
+
